9535359ebbe0bd6911a831c7cf1f0538472f9727
Tobias Koch <tobias@tobijk.de>
2014-08-31 00:21:28 +0300

    packaging: add caius-doc package

--- doc/build.sh
+++ doc/build.sh
@@ -2,6 +2,7 @@
 
 rm -fr _site
 rm -fr _api && mkdir _api
+rm -f  _manpages/*.gz
 
 # make the TCO for the tutorial
 ./gen-toc.sh
@@ -20,7 +21,11 @@ layout: api
 ---
     " > $__target
 
+    # generate HTML fragment
     ronn -5 -f --pipe $__source >> $__target
+
+    # generate man page
+    ronn --pipe -r $__source | gzip > _manpages/`basename $page .md`.3caius.gz
 done
 
 jekyll build
