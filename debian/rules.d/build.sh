ec355be51802a8fa2062295e4b4bd88e44adce87
Tobias Koch <tobias@tobijk.de>
2016-06-10 00:32:26 +0300

    remove debian package rules

--- debian/rules.d/build.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh -ex
-
-cd $XPACK_BASE_DIR/doc && ./build.sh
-
--- debian/rules.d/clean.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh -ex
-
-rm -f  $XPACK_BASE_DIR/doc/_manpages/*.gz
-rm -fr $XPACK_BASE_DIR/doc/_site
-rm -fr $XPACK_INSTALL_DIR
-
